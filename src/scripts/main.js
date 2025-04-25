26f3516a1cc951c7c95d008569eda44fda9f9440
NazarMykhailv <mihailiv1011@gmail.com>
2025-04-25 18:59:41 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
 // write your code here
+
+const theadRow = document.querySelector('thead tr');
+const ths = theadRow.querySelectorAll('th');
+
+const copiedTh = ths[1].cloneNode(true);
+
+theadRow.insertBefore(copiedTh, ths[ths.length - 1]);
+
+const rows = document.querySelectorAll('tbody tr');
+
+rows.forEach((row) => {
+  const tds = row.querySelectorAll('td');
+  const copiedTd = tds[1].cloneNode(true);
+
+  row.insertBefore(copiedTd, tds[tds.length - 1]);
+});
